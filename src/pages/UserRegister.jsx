c3860779c9802697f7fd2c8efe6c5684f9186df6
ankitsingh5423 <ankitkirola123455@gmail.com>
2025-04-05 00:28:50 +0530

    creating register page

---
+++ src/pages/UserRegister.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const UserRegister = () => {
+  return (
+    <div className="max-w-md mx-auto">
+      <h1>Sing Up</h1>
+      <p>Enter your email and password to sign up!</p>
+      <button>
+        <img src="../src/assets/src/assets/google-image.png" alt="" />
+      </button>
+    </div>
+  );
+};
+
+export default UserRegister;
