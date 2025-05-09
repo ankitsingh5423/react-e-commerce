9780603db9e0f7f9133a4a00ecd026b7ab21f41b
ankitsingh5423 <ankitkirola123455@gmail.com>
2025-05-10 00:28:32 +0530

    making inner pages

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -3,7 +3,14 @@ import testImage from "../assets/trending-products/product-1.webp";
 
 const ProductDetail = () => {
   return (
-    <div className="px-4 overflow-hidden xl:max-w-[1400px] m-auto">
+    <div className="px-4 overflow-hidden xl:max-w-[1400px] m-auto py-5">
+      <div className="py-20 text-center">
+        <h1 className="text-4xl font-semibold mb-4">Trending product</h1>
+        <p>
+          Follow the most popular trends and get exclusive items from Foesta
+          shop.
+        </p>
+      </div>
       <div className="grid grid-cols-2">
         <div>
           <img src={testImage} alt="" className="w-full" />
